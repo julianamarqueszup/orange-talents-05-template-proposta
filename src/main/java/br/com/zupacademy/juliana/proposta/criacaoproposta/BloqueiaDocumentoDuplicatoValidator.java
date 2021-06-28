6476bac625c0fef31253be55df4fd5eddbb1b8ff
julianamarqueszup <juliana.marques@zup.com.br>
2021-06-28 09:11:16 -0300

    Não pode haver proposta

---
+++ src/main/java/br/com/zupacademy/juliana/proposta/criacaoproposta/BloqueiaDocumentoDuplicatoValidator.java
@@ -0,0 +1,19 @@
+package br.com.zupacademy.juliana.proposta.criacaoproposta;
+
+import org.springframework.stereotype.Component;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Component
+public class BloqueiaDocumentoDuplicatoValidator {
+
+    @PersistenceContext
+    private EntityManager manager;
+
+    public boolean estaValido(NovaPropostaRequest request) {
+        return manager.createQuery("SELECT p.documento FROM Proposta p WHERE " +
+                "p.documento = :documento").setParameter("documento",
+                request.getDocumento()).getResultList().isEmpty();
+    }
+}
